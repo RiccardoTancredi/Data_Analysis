cc8e268b0fff1f9421d19f946023bff0d078e03a
RiccardoTancredi <ric.tancredi@gmail.com>
2020-12-10 19:25:42 +0100

    New Functions
    Now there are some new algorithms from scipy.stats and the possibility to import data from xlsx and txt

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -7,6 +7,7 @@
 import plotly as py
 import numpy as np
 import math as m
+from scipy.odr import *
 py.offline.init_notebook_mode(connected=True)
 import plotly.graph_objs as go
 
@@ -39,7 +40,7 @@ def compatibilità(x, errore_x, y, errore_y, docs:bool = False):
 # In[3]:
 
 
-def compatibilità_list(x:list, errore_x:list):
+def compatibilità_list(x:list, errore_x:list, docs:bool = False):
     """Ritorna una stringa
     """
     output = ""
@@ -48,8 +49,11 @@ def compatibilità_list(x:list, errore_x:list):
             if i == j or i-j > 0:
                 pass
             else:
-                output += f"La compatibilità tra l'elemento {i+1} e {j+1} è: \n"
-                output += compatibilità(val1,errore_x[i], val2,errore_x[j], True)           
+                output += f"La compatibilità tra l'elemento {i+1} e {j+1} è:"
+                comp = compatibilità(val1,errore_x[i], val2,errore_x[j], True)
+                output += comp
+                output += " "
+                print("La compatibilità tra l'elemento", {i+1}, "e ", {j+1}, " è:", comp)
     # while i < len(x)-1:
     #     output += f"La compatibilità tra l'elemento {i+1} e {i+2} è: \n"
     #     output += compatibilità(x[i],errore_x[i], x[i+1],errore_x[i+1])
@@ -58,7 +62,8 @@ def compatibilità_list(x:list, errore_x:list):
     #     output += f"La compatibilità tra l'elemento {j+1} e {j+2} è: \n"
     #     output += compatibilità(x[j], errore_x[j], x[j+2], errore_x[j+2])
     #     j += 1
-    return output
+    if docs: 
+        return output
 
 
 # 
@@ -67,7 +72,7 @@ def compatibilità_list(x:list, errore_x:list):
 # In[4]:
 
 
-def media_ponderata(x:list,errore_x:list, docs:bool = False):
+def media_ponderata(x:list, errore_x:list, docs:bool = False):
     """Ritorna un tuple: media, errore_media, stringa per il documento
     """
     num = 0
@@ -79,7 +84,7 @@ def media_ponderata(x:list,errore_x:list, docs:bool = False):
     media = num/den
     errore_media = 1/m.sqrt(den)
     if len(x) == 2:
-        r = compatibiltà(x[0],errore_x[0], x[1], errore_x[1])[0]
+        r = compatibilità(x[0],errore_x[0], x[1], errore_x[1])
         if r <3 :
             output += f"Poiché c'è compatibilità ne facciamo la media ponderata:\n"
     output += f"La media ponderata vale: {media}\n"
@@ -113,17 +118,32 @@ def coefficiente_Pearson(x:list, y:list, docs:bool = False):
     for l in range(len(x)):
         numeratore += (x[l]-media_x)*(y[l]-media_y)
     rho = numeratore/denominatore
-    output += f"L'indice di correlazione di Pearson vale: $\Rho = $ {rho}\n"
-    print("L'indice di correlazione di Pearson vale: $\Rho = $", rho)
+    output += f"L'indice di correlazione di Pearson vale: $\rho = $ {rho}\n"
+    print("L'indice di correlazione di Pearson vale: rho = ", rho)
     if docs:
         return output
     return rho
 
 
+# # Covarianza
+
+# In[6]:
+
+
+def covarianza(x:list, y:list):
+    N = len(x)
+    N_covariance = 0
+    x_mean = sum(x)/N
+    y_mean = sum(y)/N
+    for i in range(N):
+        N_covariance += (x[i] - x_mean)*(y[i] - y_mean)
+    return N_covariance/N
+
+
 # # INTERPOLAZIONE LINEARE  $y = a + bx $
 # 
 
-# In[6]:
+# In[7]:
 
 
 def interpolazione(y:list, errore_y:list, x:list, errore_x = None, docs:bool = False):
@@ -155,7 +175,7 @@ def interpolazione(y:list, errore_y:list, x:list, errore_x = None, docs:bool = F
     return b, errore_b, a, errore_a
 
 
-# In[7]:
+# In[8]:
 
 
 def interpolazione_no_errore(x:list, y:list, errore_y, docs:bool = False):
@@ -172,9 +192,9 @@ def interpolazione_no_errore(x:list, y:list, errore_y, docs:bool = False):
     b = (N*x_per_y-x_singoli*y_singoli)/delta
     errore_a = errore_y*m.sqrt(x_quadri/delta)
     errore_b = errore_y*m.sqrt(N/delta)
-    print("Questo è a, l'intercetta della retta: ", a,"\nQuesto il suo errore:", errore_a, "\nQuesto è b, il coefficiente angolare, cioè la velocità 1/v: ", b, "\nQuesto il suo errore:", errore_b)
+    print("Questo è a, l'intercetta della retta: ", a,"\nQuesto il suo errore:", errore_a, "\nQuesto è b, il coefficiente angolare", b, "\nQuesto il suo errore:", errore_b)
     output = ""
-    output += f"Questo è a, l'intercetta della retta: {a}\nQuesto il suo errore: {errore_a} \nQuesto è b, il coefficiente angolare, cioè la velocità 1/v: {b} \nQuesto il suo errore: {errore_b}"
+    output += f"Questo è a, l'intercetta della retta: {a}\nQuesto il suo errore: {errore_a} \nQuesto è b, il coefficiente angolare {b} \nQuesto il suo errore: {errore_b}"
     if docs:
         return output
     return (b, errore_b, a, errore_a)
@@ -183,7 +203,7 @@ def interpolazione_no_errore(x:list, y:list, errore_y, docs:bool = False):
 # 
 # # Grafico
 
-# In[8]:
+# In[9]:
 
 
 def grafico(b, a, y, errore_y, x, errore_x = None):
@@ -260,7 +280,7 @@ def grafico(b, a, y, errore_y, x, errore_x = None):
     py.offline.iplot(fig)
 
 
-# In[9]:
+# In[10]:
 
 
 def grafico_relazione(b, a, y, errore_y, x, errore_x = None):
@@ -268,10 +288,10 @@ def grafico_relazione(b, a, y, errore_y, x, errore_x = None):
     layout = go.Layout(
         title= str(input("Inserisci il nome del grafico ") ),
         yaxis=dict(
-                title = str("$"+str(input("Inserisci il titolo dell'asse y ") ) + " [" +str(input("Inserisci l'unità di misura ")+ "]" + "$"))
+                title = str("$"+str(input("Inserisci il titolo dell'asse y ") ) + " [" + str(input("Inserisci l'unità di misura ")) + "]" + "$")
         ),
         xaxis=dict(
-            title= str("$" + str(input("Inserisci il titolo dell'asse x ") ) + " [" +str(input("Inserisci l'unità di misura "))+ "]" +"$"),
+            title= str("$" + str(input("Inserisci il titolo dell'asse x ") ) + " [" +str(input("Inserisci l'unità di misura ")) + "]" +"$"),
         )
     )
     i = int(input("Scegli di quanto vuoi arrotondare b: ") )
@@ -316,7 +336,7 @@ def grafico_relazione(b, a, y, errore_y, x, errore_x = None):
             x = t,
             y = lalla,
             mode = 'lines',
-            name = str("y = " + str(round(b,i))+ "x +" + str(round(a,j)) + str( input("Inserisci l'unità di misura: ")))
+            name = str("$y = " + "(" + str(round(b,i))+ "x +" + str(round(a,j))+ ")" + str(input("Inserisci l'unità di misura ")) +"$" )
             #name = '$y = (25.594x - 0.02081) cm/s $',
     #             line = dict(
     #             shape='spline',
@@ -328,7 +348,7 @@ def grafico_relazione(b, a, y, errore_y, x, errore_x = None):
             x = t,
             y = lalla,
             mode = 'lines',
-            name = str("$y = " + "("+str(round(b,i))+ "x" + str(round(a,j)) +")" + str( input("Inserisci l'unità di misura: "))+"$")
+            name = str("$y = " + "("+str(round(b,i))+ "x" + str(round(a,j)) +")" + str(input("Inserisci l'unità di misura ")) +"$")
             #name = '$y = (25.594x - 0.02081) cm/s $',
     #             line = dict(
     #             shape='spline',
@@ -339,19 +359,25 @@ def grafico_relazione(b, a, y, errore_y, x, errore_x = None):
     py.offline.iplot(fig)
 
 
-# In[10]:
+# In[ ]:
 
 
-#z = interpolazione(velocità2, errore_velocità2, raggi_quadro2, None)
+
 
 
 # In[11]:
 
 
-#grafico(z[0], z[2], velocità2, errore_velocità2, raggi_quadro2)
+#z = interpolazione(velocità2, errore_velocità2, raggi_quadro2, None)
 
 
 # In[12]:
+
+
+#grafico(z[0], z[2], velocità2, errore_velocità2, raggi_quadro2)
+
+
+# In[13]:
 
 
 def chi_square(b, a, y:list, errore_y:list, x:list, docs:bool = False):
@@ -369,7 +395,7 @@ def chi_square(b, a, y:list, errore_y:list, x:list, docs:bool = False):
     return chi_quadro
 
 
-# In[13]:
+# In[14]:
 
 
 def errore_posteriori(b, a, y:list, x:list, docs:bool = False):
@@ -385,23 +411,32 @@ def errore_posteriori(b, a, y:list, x:list, docs:bool = False):
     return errore_a_posteriori
 
 
+# In[15]:
+
+
+def t_Student(rho, N):
+    errore_rho = m.sqrt((1-rho**2)/(N-2))
+    t = rho/errore_rho
+    print("La variabile di Student vale t =", t, "con ", N-2, "gradi di libertà")
+
+
 # # Andamento temporale
 
-# In[14]:
+# In[16]:
 
 
-def andamento_temporale_relazione(b, a, y:list, errore_y, x:list):
+def andamento_temporale_relazione(b, a, y:list, errore_y, x:list, x_axis_name = "Occorrenze" ,colore = "#EF553B"):
     scarti = []
     #vincoli = N-2 == 9
     for i in range(0,len(y)):
-        scarti.append((y[i]-(b*x[i]+a))/errore_y)
+        scarti.append((y[i]-(b*x[i]+a))/errore_y[i])
     layout = go.Layout(
         title= "",
         yaxis=dict(
                 title = 'Scarti'
         ),
         xaxis=dict(
-            title= str("$" + str(input("Inserisci il titolo dell'asse x: ")) + "$")
+            title= str("$" + x_axis_name + "$")
         )
     )
     l = np.linspace(0,99,100)
@@ -413,7 +448,8 @@ def andamento_temporale_relazione(b, a, y:list, errore_y, x:list):
         #y = 7.98x -1.1
         line = dict(
         #shape='spline',
-        color = str(input("Inserisci un colore (default #EF553B): "))
+#         color = str(input("Inserisci un colore (default #EF553B): "))
+        color = str(colore)
                    ),
         )
 #     print(traccia)   
@@ -436,14 +472,14 @@ def andamento_temporale_relazione(b, a, y:list, errore_y, x:list):
     fig.show()
 
 
-# In[15]:
+# In[17]:
 
 
-def andamento_temporale(b, a, y:list, errore_y, x:list):
+def andamento_temporale(b, a, y:list, errore_y:list, x:list):
     scarti = []
     #vincoli = N-2 == 9
     for i in range(0,len(y)):
-        scarti.append((y[i]-(b*x[i]+a))/errore_y)
+        scarti.append((y[i]-(b*x[i]+a))/errore_y[i])
     layout = go.Layout(
         title= "",
         yaxis=dict(
@@ -481,15 +517,15 @@ def andamento_temporale(b, a, y:list, errore_y, x:list):
     fig.show()
 
 
-# In[16]:
+# In[18]:
 
 
-def traccia(y, errore_y, x, errore_x = None):
+def traccia(y, errore_y, x, errore_x = None, modo = 'markers'):
     #l = np.linspace(0,99,100)
     traccia1 = go.Scatter(
         x = x,
         y = y,
-        mode = 'markers',
+        mode = str(modo),
         name= "$x_i$",
         showlegend=True,
         error_y=dict(
@@ -502,34 +538,26 @@ def traccia(y, errore_y, x, errore_x = None):
         ),
     )
     return traccia1
+
 def retta(b, a, y, errore_y, x, errore_x = None):
-    t = np.linspace(0., max(x), 1000)
+    max_x, min_x = max(x), min(x)
+    t_max = max_x if max_x >= abs(min_x) else min_x
+    t = np.linspace(-t_max, t_max, 1000) 
     lalla = a + b*t
-    if a >= 0:
-        retta1 = go.Scatter(
-            x = t,
-            y = lalla,
-            mode = 'lines',
-            name = "Retta"
-            #name = '$y = (25.594x - 0.02081) cm/s $',
-    #             line = dict(
-    #             shape='spline',
-    #             color = 'orange'
-    #        )  
-        )
-    else:
-        retta1 = go.Scatter(
-            x = t,
-            y = lalla,
-            mode = 'lines',
-            name = "Retta"
-            #name = '$y = (25.594x - 0.02081) cm/s $',
-    #             line = dict(
-    #             shape='spline',
-    #             color = 'orange'
-    #        )  
-        )
+    retta1 = go.Scatter(
+        x = t,
+        y = lalla,
+        mode = 'lines',
+        name = "Retta"
+        #name = '$y = (25.594x - 0.02081) cm/s $',
+#             line = dict(
+#             shape='spline',
+#             color = 'orange'
+#        )  
+    )
+
     return retta1
+
 def Layout():
     layout = go.Layout(
         title= "",
@@ -544,7 +572,13 @@ def Layout():
     
 
 
-# In[17]:
+# In[ ]:
+
+
+
+
+
+# In[19]:
 
 
 def traccia_relazione(y, errore_y, x, errore_x = None):
@@ -569,7 +603,9 @@ def traccia_relazione(y, errore_y, x, errore_x = None):
 def retta_relazione(b:float, a:float, y, errore_y, x, errore_x = None):
     i = int(input("Scegli di quanto vuoi arrotondare b: ") )
     j = int(input("Scegli di quanto vuoi arrotondare a: ") )
-    t = np.linspace(0., max(x), 1000)
+    max_x, min_x = max(x), min(x)
+    t_max = max_x if max_x >= abs(min_x) else min_x
+    t = np.linspace(-t_max, t_max, 1000)
     lalla = float(round(a,j)) + float( round(b,i))*t
     if a >= 0:
         retta1 = go.Scatter(
@@ -611,7 +647,7 @@ def Layout_relazione():
     
 
 
-# In[18]:
+# In[20]:
 
 
 def Grafico(x:list, layout):
@@ -619,9 +655,78 @@ def Grafico(x:list, layout):
     fig.show()
 
 
+# # Fit Lineare
+
+# # $y = bx + a$
+
+# In[21]:
+
+
+def f(B, x):
+    '''Linear function y = m*x + b'''
+    # B is a vector of the parameters.
+    # x is an array of the current x values.
+    # x is in the same format as the x passed to Data or RealData.
+    #
+    # Return an array in the same format as y passed to Data or RealData.
+    return B[0]*x + B[1]
+def linear_fit(x, y, sigma_x, sigma_y, b, a):
+    mydata = RealData(x, y, sx=sigma_x, sy=sigma_y)
+    linear = Model(f)
+    myodr = ODR(mydata, linear, beta0=[b, a])
+    myoutput = myodr.run()
+    myoutput.pprint()
+    return myoutput.beta[0], myoutput.sd_beta[0], myoutput.beta[1], myoutput.sd_beta[1]
+
+
+# In[ ]:
+
+
+
+
+
+# # Fit Parabolico
+
+# # $y = ax^2 + bx + c$
+
+# In[22]:
+
+
+def g(B, x):
+    '''quadratic function y = a*x**2 + b*x + c'''
+    # B is a vector of the parameters.
+    # x is an array of the current x values.
+    # x is in the same format as the x passed to Data or RealData.
+    #
+    # Return an array in the same format as y passed to Data or RealData.
+    return B[0]*x**2 + B[1]*x + B[2]
+def parabolic_fit(x, y, sigma_x, sigma_y, a, b, c):
+    mydata = RealData(x, y, sx=sigma_x, sy=sigma_y)
+    quadratic = Model(g)
+    myodr = ODR(mydata, quadratic, beta0=[a, b, c])
+    myoutput = myodr.run()
+    myoutput.pprint()
+    return myoutput.beta[0], myoutput.sd_beta[0], myoutput.beta[1], myoutput.sd_beta[1], myoutput.beta[2], myoutput.sd_beta[2]
+
+
+# In[23]:
+
+
+def max_quadratic(a, b, c):
+    x_max = -b/(2*a)
+    y_max = a*x_max**2 + b*x_max +c
+    return (y_max, x_max)
+
+
+# In[ ]:
+
+
+
+
+
 # # Scrittura su file
 
-# In[19]:
+# In[24]:
 
 
 def scrittura(nome_file, contenuto):
@@ -641,4 +746,50 @@ def scrittura_aggiunta(nome_file, contenuto):
     a_capo = "\n"
     f.write(a_capo)
     f.close()
+
+
+# # Importare da Excel
+
+# In[25]:
+
+
+import openpyxl
+def excel_import(nome_file:str, start, stop, col):
+    #excel_document = openpyxl.load_workbook('Guidovia.xlsx')
+    #sheet = excel_document.get_sheet_by_name('Foglio1')
+    nome_file += '.xlsx'
+    wb = openpyxl.load_workbook(nome_file)
+    sheets = wb.sheetnames
+    ws = wb[sheets[0]]
+    vettore = [] #array contenete le medie delle misure effettuate della variabile diretta.
+    for i in range(start, stop + 1):
+        casella = ws.cell(row=i, column=col)
+        vettore.append(casella.value) #aggiunge elementi al vettore (array)
+    return vettore
+
+
+# # Leggi file
+
+# In[26]:
+
+
+def text(nome_file:str):
+    h = open(nome_file, 'r')
+    # Reading from the file
+    content = h.readlines()
+    # print(content)
+    h.close()
+    n = 0
+    x = []
+    y = []
+    for line in content:
+        if n==0:
+            N = line[0]
+        if n>0:
+            words = line.split()
+            if len(words):
+                x.append(words[0])
+                y.append(words[1])
+        n+=1
+    return(x,y)
 
